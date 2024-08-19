1d6828b6be48594b56b20b9110fb1c5556a210e5
ye hyang <68382133+sylrose@users.noreply.github.com>
2024-08-18 20:00:35 -0400

    df

---
+++ js/comic_footer.js
@@ -0,0 +1,9 @@
+//the footer of the site would be handled in this javascript file, so you don't have to copypaste the whole thing onto every page.
+//at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeFooter"
+document.querySelector(".writeFooter").innerHTML = `
+    <footer align="center">
+        <p>this comic is made by MICHA YE-HYANG F. dont steal my art ples</p>
+        <p><strong>Powered by</strong></p>
+        <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" /></a>
+    </footer>
+`;
